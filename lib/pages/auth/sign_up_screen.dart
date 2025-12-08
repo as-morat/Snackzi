be6b6e18a0795eb6a94abbabba03365cef5cfe8a
as-morat <morat9511@gmail.com>
2025-12-08 16:56:44 +0600

    add widget for custom button & update signup screen

--- lib/pages/auth/sign_up_screen.dart
+++ lib/pages/auth/sign_up_screen.dart
@@ -1,16 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/my_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isChecked = false;
+
+  final baseText = GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: .w500,
+    color: Colors.grey,
+  );
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: .all(15), child: Column(
-        children: [
-          Image.asset("images/6343825.jpg", width: 500, fit: .cover,)
-        ],
-      )),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: .all(15),
+        child: Column(
+          children: [
+            // Intro Images
+            Image.asset("images/6343825.jpg", width: 500, fit: .cover),
+            const SizedBox(height: 20),
+            // Input Fields for email
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Input Fields for Password
+            TextField(
+              controller: passwordController,
+              obscureText: !isChecked,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isChecked = !isChecked;
+                    });
+                  },
+                  icon: Icon(
+                    isChecked ? Icons.visibility : Icons.visibility_off,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            // Sign Up Button
+            SizedBox(
+              child: MyButton(onTap: () {}, buttonText: "Sign Up"),
+            ),
+            const SizedBox(height: 20),
+            // Already have an account?
+            Row(
+              mainAxisAlignment: .center,
+              mainAxisSize: .min,
+              children: [
+                Text("Already have an account?", style: baseText),
+                const SizedBox(width: 5),
+                Text(
+                  "Login here",
+                  style: baseText.copyWith(
+                    fontWeight: .bold,
+                    color: Colors.blue,
+                    letterSpacing: -1,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String buttonText;
+  final Color? color;
+  const MyButton({
+    super.key,
+    required this.onTap,
+    required this.buttonText,
+    this.color = Colors.blueAccent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const .symmetric(vertical: 12, horizontal: 24),
+        backgroundColor: color,
+        shape: RoundedRectangleBorder(borderRadius: .circular(10)),
+      ),
+      child: Text(
+        buttonText,
+        style: GoogleFonts.poppins(
+          fontSize: 18,
+          fontWeight: .w600,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
