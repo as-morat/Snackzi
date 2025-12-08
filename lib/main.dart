92d87c58fcc1b9598abfc9f0c7554dec8300e023
as-morat <morat9511@gmail.com>
2025-12-08 15:46:06 +0600

    update .gitignore & setup supabase environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+final supabaseUrl = dotenv.env["SUPABASE_URL"] ?? "";
+final supabaseKey = dotenv.env["SUPABASE_ANON_KEY"] ?? "";
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+    );
   }
 }
