a8128c157404469c15db5ed691c7fdb4da8e0ec3
as-morat <morat9511@gmail.com>
2025-12-09 10:19:32 +0600

    create custom widget method for navigation items

--- lib/pages/screens/home_screen.dart
+++ lib/pages/screens/home_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 90,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Row(mainAxisAlignment: .spaceEvenly, children: []),
+      ),
+    );
+  }
 
+  // helper method to build navigation items
+  Widget _buildNavItems(IconData icon, String label, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {});
+      },
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 28,
+            color: currentIndex == index ? red : Colors.grey,
+          ),
+          const SizedBox(height: 3),
+          CircleAvatar(
+            radius: 3,
+            backgroundColor: currentIndex == index ? red : Colors.transparent,
+          ),
+        ],
+      ),
     );
   }
 }
