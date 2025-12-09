a1fa0325623395e195142f1fd34aa7b931976e66
as-morat <morat9511@gmail.com>
2025-12-09 10:28:21 +0600

    add custom bottom nav item widget

--- lib/pages/screens/home_screen.dart
+++ lib/pages/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,7 +18,42 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: Container(
         height: 90,
         decoration: BoxDecoration(color: Colors.white),
-        child: Row(mainAxisAlignment: .spaceEvenly, children: []),
+        child: Row(
+          mainAxisAlignment: .spaceEvenly,
+          children: [
+            _buildNavItems(Iconsax.home_15, "Home", 0),
+            const SizedBox(width: 10),
+            _buildNavItems(Iconsax.heart, "Favourite", 1),
+            const SizedBox(width: 90),
+            _buildNavItems(Icons.person_2_outlined, "Profile", 2),
+            const SizedBox(width: 10),
+            Stack(
+              clipBehavior: .none,
+              children: [
+                _buildNavItems(Iconsax.shopping_cart, "Cart", 3),
+                Positioned(
+                  right: -7,
+                  top: 16,
+                  child: CircleAvatar(
+                    backgroundColor: red,
+                    child: Text(
+                      "0",
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 155,
+                    top: -25,
+                    child: CircleAvatar(
+                  backgroundColor: red,
+                  radius: 35,
+                  child: Icon(CupertinoIcons.search, size: 35, color: Colors.white,,),
+                ))
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
