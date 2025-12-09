cf3b53fc1f1b55a8cfbb638c04b59afee68c0d1c
as-morat <morat9511@gmail.com>
2025-12-09 10:33:42 +0600

    add navigation items screens

--- lib/pages/screens/app_main_screen.dart
+++ lib/pages/screens/app_main_screen.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/screens/home_screen.dart';
+import 'package:food_delivery/pages/screens/profile_screen.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:iconsax/iconsax.dart';
 
 class AppMainScreen extends StatefulWidget {
   const AppMainScreen({super.key});
@@ -8,8 +13,87 @@ class AppMainScreen extends StatefulWidget {
 }
 
 class _AppMainScreenState extends State<AppMainScreen> {
+  int currentIndex = 0;
+  final List<Widget> _pages = [
+    HomeScreen(),
+    Scaffold(),
+    ProfileScreen(),
+    Scaffold()
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[currentIndex],
+      bottomNavigationBar: Container(
+        height: 90,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Row(
+          mainAxisAlignment: .spaceEvenly,
+          children: [
+            _buildNavItems(Iconsax.home_15, "Home", 0),
+            const SizedBox(width: 10),
+            _buildNavItems(Iconsax.heart, "Favourite", 1),
+            const SizedBox(width: 90),
+            _buildNavItems(Icons.person_2_outlined, "Profile", 2),
+            const SizedBox(width: 10),
+            Stack(
+              clipBehavior: .none,
+              children: [
+                _buildNavItems(Iconsax.shopping_cart, "Cart", 3),
+                Positioned(
+                  right: -7,
+                  top: 16,
+                  child: CircleAvatar(
+                    backgroundColor: red,
+                    child: Text(
+                      "0",
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 155,
+                  top: -25,
+                  child: CircleAvatar(
+                    backgroundColor: red,
+                    radius: 35,
+                    child: Icon(
+                      CupertinoIcons.search,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // helper method to build navigation items
+  Widget _buildNavItems(IconData icon, String label, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 28,
+            color: currentIndex == index ? red : Colors.grey,
+          ),
+          const SizedBox(height: 3),
+          CircleAvatar(
+            radius: 3,
+            backgroundColor: currentIndex == index ? red : Colors.transparent,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/screens/home_screen.dart
+++ lib/pages/screens/home_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:food_delivery/utils/colors.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,69 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: 90,
-        decoration: BoxDecoration(color: Colors.white),
-        child: Row(
-          mainAxisAlignment: .spaceEvenly,
-          children: [
-            _buildNavItems(Iconsax.home_15, "Home", 0),
-            const SizedBox(width: 10),
-            _buildNavItems(Iconsax.heart, "Favourite", 1),
-            const SizedBox(width: 90),
-            _buildNavItems(Icons.person_2_outlined, "Profile", 2),
-            const SizedBox(width: 10),
-            Stack(
-              clipBehavior: .none,
-              children: [
-                _buildNavItems(Iconsax.shopping_cart, "Cart", 3),
-                Positioned(
-                  right: -7,
-                  top: 16,
-                  child: CircleAvatar(
-                    backgroundColor: red,
-                    child: Text(
-                      "0",
-                      style: TextStyle(fontSize: 12, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 155,
-                    top: -25,
-                    child: CircleAvatar(
-                  backgroundColor: red,
-                  radius: 35,
-                  child: Icon(CupertinoIcons.search, size: 35, color: Colors.white,,),
-                ))
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
-  // helper method to build navigation items
-  Widget _buildNavItems(IconData icon, String label, int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {});
-      },
-      child: Column(
-        children: [
-          Icon(
-            icon,
-            size: 28,
-            color: currentIndex == index ? red : Colors.grey,
-          ),
-          const SizedBox(height: 3),
-          CircleAvatar(
-            radius: 3,
-            backgroundColor: currentIndex == index ? red : Colors.transparent,
-          ),
-        ],
-      ),
     );
   }
 }
